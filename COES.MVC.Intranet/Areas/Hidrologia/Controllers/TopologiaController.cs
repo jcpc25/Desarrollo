054eed3f69c10ee183ee9eae3d49bca3eed1cd0a
jcpc25 <jcpc25@hotmail.com>
2015-05-16 15:49:56 -0500

    2015-05-16 topologia

---
+++ COES.MVC.Intranet/Areas/Hidrologia/Controllers/TopologiaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COES.MVC.Intranet.Areas.Hidrologia.Controllers
+{
+    public class TopologiaController : Controller
+    {
+        //
+        // GET: /Hidrologia/Topologia/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+    }
+}
